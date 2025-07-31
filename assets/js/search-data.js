ef32556f8231979079c3131f539ae0ef16e6b85d
savannahswinea <savannah.swinea@gmail.com>
2025-07-31 18:44:48 +0000

    Deploying to gh-pages from @ savannahswinea/savannahswinea.github.io@e6513f61bd61460ebefa2a5718fac8deaf4bf0a4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -159,9 +159,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-shifting-habitats-and-ecosystem-services",
+          title: 'Shifting habitats and ecosystem services',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
