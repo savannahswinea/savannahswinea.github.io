334dc9c1b08cee3e01f1a028bbd9483e92254a2f
savannahswinea <savannah.swinea@gmail.com>
2025-10-15 18:13:57 +0000

    Deploying to gh-pages from @ savannahswinea/savannahswinea.github.io@e55b6358a1392c0e3c17b0b01ad368249814bea9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -160,7 +160,17 @@ ninja.data = [{
           section: "News",},{id: "news-first-day-of-my-postdoc-at-nc-state-wolf",
           title: 'First day of my postdoc at NC State :wolf:',
           description: "",
-          section: "News",},{id: "projects-shifting-habitats-and-ecosystem-services",
+          section: "News",},{id: "projects-fish-and-fisheries-resilience-to-disturbances",
+          title: 'Fish and fisheries resilience to disturbances',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-social-dynamics-of-landscape-change",
+          title: 'Social dynamics of landscape change',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-shifting-habitats-and-ecosystem-services",
           title: 'Shifting habitats and ecosystem services',
           description: "",
           section: "Projects",handler: () => {
@@ -185,16 +195,6 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "projects-fish-and-fisheries-resilience-to-disturbances",
-          title: 'Fish and fisheries resilience to disturbances',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/fishdisturbance/";
-            },},{id: "projects-social-dynamics-of-landscape-change",
-          title: 'Social dynamics of landscape change',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/residentsmangroves/";
             },},{
         id: 'social-email',
         title: 'email',
